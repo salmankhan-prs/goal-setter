dff5d622eb827e0efd2fda12ff7c1bb244f01eda
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2022-02-21 17:09:03 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/user";
+const API_URL = "https://goals-backend-app.herokuapp.com/api/user";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
