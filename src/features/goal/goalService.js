49b34650180a4832a59ff59a8b6c661775068d2d
PATHAN SALMAN KHAN <54095769+salmankhan-prs@users.noreply.github.com>
2022-02-21 17:10:20 +0530

    Update goalService.js

--- src/features/goal/goalService.js
+++ src/features/goal/goalService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import toast from "react-hot-toast";
-const API_URL = "http://localhost:5000/api/goals";
+const API_URL = "https://goals-backend-app.herokuapp.com/api/goals";
 
 const getGoals = async (token) => {
   const config = {
